5ff4f36d58c7386907c2a8c8a323c1263682dc8e
Marco Enrico Piras <kikkomep@crs4.it>
2023-05-11 10:44:38 +0200

    fix(gh ctrl): ensure that sender and target are registered users

--- lifemonitor/tasks/jobs/github.py
+++ lifemonitor/tasks/jobs/github.py
@@ -4,7 +4,6 @@ import logging
 from apscheduler.triggers.cron import CronTrigger
 
 from lifemonitor.auth.models import User
-from lifemonitor.auth.oauth2.client.models import OAuthIdentity
 from lifemonitor.integrations.github import LifeMonitorGithubApp
 from lifemonitor.integrations.github.controllers import get_event_handler
 from lifemonitor.integrations.github.events import GithubEvent
@@ -33,19 +32,41 @@ def handle_event(event):
     logger.debug("Github event: %r", e)
     logger.debug("Github event type: %r", e.type)
     logger.debug("Github event installation: %r", e.installation)
-    logger.debug("Github event repository reference: %r", e.repository_reference)
-    logger.debug("Github event repository reference branch: %r", e.repository_reference.branch)
-    logger.debug("Github event repository reference tag: %r", e.repository_reference.tag)
-    logger.debug("Github event repository owner: %r", e.repository_reference.owner_id)
+    logger.debug("Github installation target id: %r", e.installation.target_id)
+    logger.debug("Github installation target type: %r", e.installation.target_type)
+
+    try:
+        # use the try block to avoid exceptions when the event is not related to a single repository
+        # (i.e. the installation event)
+        logger.debug("Github event repository reference: %r", e.repository_reference)
+        logger.debug("Github event repository reference branch: %r", e.repository_reference.branch)
+        logger.debug("Github event repository reference tag: %r", e.repository_reference.tag)
+        logger.debug("Github event repository owner: %r", e.repository_reference.owner_id)
+    except ValueError as ex:
+        logger.debug("Unable to get repository reference from event: %r", ex)
 
     # find user by provider user id
-    installation_owner = OAuthIdentity.find_by_provider_user_id(str(e.repository_reference.owner_id), 'github')
-    logger.debug("User of the installed repository: %r", installation_owner)
-    if not installation_owner:
-        logger.warning("No user found for the installed repository")
+    installation_sender = e.sender_as_user
+    logger.debug("Installation sender: %r", installation_sender)
+    if not installation_sender:
+        logger.warning(f"No LifeMonitor user associated to the GitHub sender {e.sender_id} for the repository")
         return
 
-    gh_settings: GithubUserSettings = installation_owner.user.github_settings
+    logger.debug("Installation sender ID: %r", installation_sender.username)
+
+    # check if the target user is an organization and if so, ensure that the organization is mapped to a LifeMonitor user
+    if e.installation.target_type == "Organization":
+        gh_app = LifeMonitorGithubApp.get_instance()
+        org = gh_app.get_organization(e.installation.target_id)
+        # return "ok", 200
+        logger.debug("Organization: %r", org)
+        if not org:
+            org = gh_app.register_organization(e.installation.account._rawData)
+        if not org:
+            logger.warning("Unable to find or register the organization")
+            return "Unable to find or register the organization", 400
+
+    gh_settings: GithubUserSettings = installation_sender.github_settings
     logger.debug("Github settings: %r", gh_settings)
     if not gh_settings:
         logger.warning("No GitHub settings found for the installed repository")
